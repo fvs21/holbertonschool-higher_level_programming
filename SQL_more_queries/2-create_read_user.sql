1526b0f1f09d5e04c31b9d17dfa3faaabcac0e65
Fabrizio Vanzani <vanzanifabrizio@gmail.com>
2026-02-22 19:07:07 -0600

    jdmksla

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
--- 2
+-- Create db
+-- Create user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 DROP USER IF EXISTS 'user_0d_2'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
